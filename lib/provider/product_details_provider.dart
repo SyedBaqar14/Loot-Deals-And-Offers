8557f22cf5a7c7cba2458961618099f07b97499b
Syed Hassan <hassan_baqar@outlook.com>
2022-03-30 15:36:54 +0500

    Update product_details_provider.dart

--- lib/provider/product_details_provider.dart
+++ lib/provider/product_details_provider.dart
@@ -22,7 +22,7 @@ class DetailsPageProvider with ChangeNotifier {
       final response = await http.get(Uri.parse(url),
         headers: <String, String> {
           'Accept': 'application/json',
-          'Authorization': 'Bearer NmtjejlSSXFPOWtMZ2J2VVlBSnF6MFdGUG9OMzQrRVVib2UzaUFwUXQ1UnhKZjRRV3AyTWlaR2VFOEhBY1Freg=='
+          'Authorization': 'Bearer xxx'
         },
       );
 
